32331dd21bac9ab13b648fed5174f99811e195c2
Brian Zauner <binrz00@hotmail.com>
2020-02-04 21:32:02 -0600

    intial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// import axios from "axios";
 import App from "./App";
-
+//Health Check
+// axios.get("http://localhost:5000/ping").then(()=>{
+//     console.log("should be working")
+// }).catch(()=>{
+//     console.log("something is wrong")
+// })
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
