d9af58e9d7f84de728fc8b392da26a28b1c8decb
Brian Zauner <binrz00@hotmail.com>
2020-02-10 14:42:57 -0600

    ping
    ping

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import axios from "axios";
+import axios from "axios";
 import App from "./App";
 
-//Health Check
-// axios.get("http://localhost:5000/ping").then(()=>{
-//     console.log("should be working")
-// }).catch(()=>{
-//     console.log("something is wrong")
-// })
+// Health Check
+axios
+  .get(process.env.REACT_APP_URL + "/ping")
+  .then(() => {
+    console.log("should be working");
+  })
+  .catch(() => {
+    console.log("something is wrong");
+  });
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
